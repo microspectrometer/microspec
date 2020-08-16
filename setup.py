03e011ca546f7b8b8f096ad4267af227e9f0e457
sustainablelab <sustainablelab@gmail.com>
2020-08-16 12:08:43 -0400

    Increment PyPI version

--- setup.py
+++ setup.py
@@ -46,8 +46,8 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    # version='0.1.1a5',  # Required (reset to this before PyPI publish)
-    version='0.1.1a13',  # Temporary increment for TestPyPI publish)
+    version='0.1.1a5',  # Required (reset to this before PyPI publish)
+    # version='0.1.1a13',  # Temporary increment for TestPyPI publish)
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
