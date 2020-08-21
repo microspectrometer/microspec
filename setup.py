6654e94f425f1ad0d55b8e12f8bfb68543a15873
sustainablelab <sustainablelab@gmail.com>
2020-08-21 15:55:46 -0400

    Increment PyPI version number

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.1a7',  # Required (reset to this before PyPI publish)
+    version='0.1.1a8',  # Required (reset to this before PyPI publish)
     # version='0.1.1a13',  # Temporary increment for TestPyPI publish)
 
     # This is a one-line description or tagline of what your project does. This
