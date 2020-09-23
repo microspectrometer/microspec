51570d8c1bd61dbf6809d37f85951b75787357c3
sustainablelab <sustainablelab@gmail.com>
2020-09-23 18:05:49 -0400

    Fix missing JSON cfg bug on Linux with Python in ~/.local instead of /usr/local

--- src/microspeclib/internal/configutil.py
+++ src/microspeclib/internal/configutil.py
@@ -4,16 +4,19 @@
 
 from microspeclib        import CHROMASPEC_ROOTDIR
 from microspeclib.logger import CHROMASPEC_LOGGER_JSON as log
-import os, sys
+import os, sys, site
 
 def findConfig(filename):
   log.info("filename=%s", filename)
   developer = os.path.join(CHROMASPEC_ROOTDIR, "cfg", filename)
   installed = os.path.join(sys.prefix, "share", "microspeclib", "cfg", filename)
+  altinstalled = os.path.join(site.USER_BASE, "share", "microspeclib", "cfg",
+          filename)
   local     = os.path.join(".", filename)
   found     = None
   if(  os.access(developer, os.R_OK)): found = developer;
   elif(os.access(installed, os.R_OK)): found = installed;
+  elif(os.access(altinstalled, os.R_OK)): found = altinstalled;
   elif(os.access(local,     os.R_OK)): found = local;
   else: raise Exception("Cannot find config file %s"%(filename))
   return found
